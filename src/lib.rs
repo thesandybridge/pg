e94f2ca2e88fe19b27e59f97f7138046c6733085
Matt Miller <mattmillerf@pm.me>
2023-05-04 11:46:44 -0400

    add support for commas

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
 use anyhow::Result;
 
-pub fn percentage_of_growth(initial_value: &f64, final_value: &f64) -> Result<f64> {
+pub fn percentage_of_growth(initial_value: f64, final_value: f64) -> Result<f64> {
     Ok(((final_value - initial_value) / initial_value) * 100.00)
+}
+
+pub fn parse_with_commas(input: &String) -> Result<f64> {
+    let without_commas = input.replace(",", "");
+    Ok(without_commas
+       .parse::<f64>()
+       .unwrap_or_else(|_| panic!("Invalid number format: {}", input))
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ fn cli() -> Command {
     Command::new("pg")
         .about("Calculate percentage of growth")
         .subcommand_required(false)
-        .arg(arg!(<INITIAL_VALUE> "The initial value").required(true).value_parser(clap::value_parser!(f64)))
-        .arg(arg!(<FINAL_VALUE> "The final value").required(true).value_parser(clap::value_parser!(f64)))
+        .arg(arg!(<INITIAL_VALUE> "The initial value").required(true))
+        .arg(arg!(<FINAL_VALUE> "The final value").required(true))
 }
 
 fn main() -> Result<()>{
     let command = cli().get_matches();
 
-    let initial_value = command.get_one::<f64>("INITIAL_VALUE").unwrap();
-    let final_value = command.get_one::<f64>("FINAL_VALUE").unwrap();
+    let initial_value = pg::parse_with_commas(command.get_one("INITIAL_VALUE").unwrap())?;
+    let final_value = pg::parse_with_commas(command.get_one("FINAL_VALUE").unwrap())?;
 
     let result = pg::percentage_of_growth(initial_value, final_value)?;
 
